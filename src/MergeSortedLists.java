4d3d4bb94af95a207f64885b6fad2e4fb932e4c3
Abdullahi Abdukadir <abdallaabdukadir256@gmail.com>
2024-10-15 15:45:20 +0300

    Postman test

---
+++ src/MergeSortedLists.java
@@ -0,0 +1,60 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MergeSortedLists {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to help with merging
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // Traverse both lists and add smaller values to the merged list
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        printList(mergedList);
+    }
+}
